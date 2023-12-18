c14a1769a039b19dcdcaf7006b4b23afefb374f8
VincentBeaupre <95039252+vincentbeaupre@users.noreply.github.com>
2023-12-18 12:09:00 -0500

    Update api.js

--- wwwroot/PhotosManager/js/api.js
+++ wwwroot/PhotosManager/js/api.js
@@ -3,7 +3,7 @@
 
 ////////////////////////////////////////////// API photos_APIs call ///////////////////////////////////////////////////////
 
-const serverHost = "http://localhost:5000";
+const serverHost = "https://regular-resisted-spike.glitch.me";
 const photos_API = "/api/photos";
 const photoLikes_API = "/api/likes";
 class API {
